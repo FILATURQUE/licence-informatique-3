dac0cb8a73582448bbc3858a0ad15969750c66ba
FILATURQUE <asphyxytb@gmail.com>
2025-12-04 13:16:45 +0100

    asd

--- SystemInfo/TM/TM2/ex1.c
+++ SystemInfo/TM/TM2/ex1.c
@@ -4,28 +4,23 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    for (int i = 2; i < argc; i++)
+    pid_t pid = fork();
+
+    if (pid < 0)
     {
-        pid_t pid = fork();
-
-        if (pid < 0)
-        {
-            perror("fork echoue");
-            exit(1);
-        }
-        
-        if (pid == 0){
-
-            execlp(argv[1],argv[i]);
-            perror("execlp");
-            _exit(1);
-
-        }
-        wait(NULL);
+        perror("fork echoue");
+        exit(1);
     }
     
-    
+    if (pid == 0){
+
+        execlp("date","date",NULL);
+        perror("execlp");
+        _exit(1);
+
+    }
+    wait(NULL); 
     return 0;
 }
